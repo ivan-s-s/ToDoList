1cd2f00c0a4d00ca415608f510f4500855275f77
surik.v <i.surovtsev22@gmail.com>
2024-03-18 21:23:09 +0500

    Add event listener and render function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 import './styles/main.css';
 
+import { newProjectButton, renderProjectForm } from './projectForm';
+import { projectContainer, renderProjects } from './projectList';
+import { addProject } from './project';
+
+
+
+newProjectButton.addEventListener('click', () => {
+  if(projectContainer.nextElementSibling != newProjectButton) {
+    return
+  }
+  const form = renderProjectForm();
+  form.createButton.addEventListener('click', () => {
+    const element = document.getElementById('project-name-input')
+    const name = element.value
+    if (!name) {
+      console.log('Enter the name of your project pls!')
+    } else {
+      addProject(name)
+      renderProjects()
+      element.value = ''
+    }
+  })
+  form.cancleButton.addEventListener('click', () => {
+    form.newProjectForm.remove()
+  })
+})
+
+renderProjects();
 // const div = document.createElement('div');
 // div.innerHTML = 'Hello';
 // div.classList.add('hello');
