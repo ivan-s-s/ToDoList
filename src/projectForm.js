06894f3a24706e2363e508970e313c0fb7f23d66
surik.v <i.surovtsev22@gmail.com>
2024-03-18 21:16:36 +0500

    Create render form function

---
+++ src/projectForm.js
@@ -0,0 +1,23 @@
+import { createLabel, createInput } from './modules/createInput';
+import { createButton } from './modules/createButton';
+
+export const newProjectButton = document.querySelector('#new-project-button');
+// создание формы
+export function renderProjectForm() {
+  const newProjectForm = document.createElement('div');
+  newProjectForm.id = 'new-project-form';
+  newProjectButton.before(newProjectForm);
+
+  createLabel(newProjectForm, 'project-name-label', 'project-name-input', 'Project Name: ')
+  createInput(newProjectForm, 'text', 'project-name-input', 'input-text')
+
+  const createButtonForm = createButton(newProjectForm, "new-project-create-button-id", "new-project-create-button-class", "Create");
+
+  const cancelButtonForm = createButton(newProjectForm, "new-project-cancel-button-id", "new-project-cancel-button-class", "Cancel");
+
+  return {
+    newProjectForm,
+    createButton: createButtonForm,
+    cancleButton: cancelButtonForm
+  }
+}
