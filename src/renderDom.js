5f9a3d3236c6340d11ca0f5eff4551f13f8742b6
surik.v <i.surovtsev22@gmail.com>
2024-03-18 22:16:34 +0500

    Create remove buttons listener function

--- src/renderDom.js
+++ src/renderDom.js
@@ -1,3 +1,6 @@
+import { deleteProject } from "./project";
+import { projectContainer, renderProjects } from "./projectList";
+
 export let index = 0;
 export let taskIndex = 0;
 
@@ -34,4 +37,24 @@ export function createRadioInput(inputClass, parentElement, valueArray) {
   parentElement.append(elementContainer)
 
 
+}
+
+// функция удаления
+export function getRemoveButtons(container) {
+  let removeButtons = container.querySelectorAll('.remove-button')
+  const removeButtonArray = Array.from(removeButtons)
+  removeButtons.forEach(button => {
+      button.addEventListener('click', () => {
+          if(container === projectContainer) {
+              index = removeButtonArray.indexOf(button)
+              deleteProject(index)
+          }
+          else if(container === taskContainer) {
+              taskIndex = removeButtonArray.indexOf(button)
+              projectList[index].deleteTask(taskIndex)
+          }
+          renderProjects()
+          // renderTasks()
+      })
+  })
 }
