28c9f06f8c2772934ed13b1fc65b6d80b12829c3
surik.v <i.surovtsev22@gmail.com>
2024-03-19 23:40:16 +0500

    Add event listener as a get remove buttons function

--- src/renderDom.js
+++ src/renderDom.js
@@ -1,59 +1,65 @@
 import { deleteProject } from "./project";
-import { projectContainer, renderProjects } from "./projectList";
+import { projectContainer, renderProjectEdit, renderProjects } from "./projectList";
 
 export let index = 0;
 export let taskIndex = 0;
 
-export function createLabel(text, inputId, labelId, parentElement) {
-  const inputLabel = document.createElement('label');
-  inputLabel.htmlFor = inputId;
-  inputLabel.id = labelId;
-  inputLabel.textContent = text;
-  parentElement.append(inputLabel);
-}
 
-export function createInput(
-  type,
-  inputId,
-  parentElement
-) {
-  const inputElement = document.createElement('input');
-  inputElement.type = type;
-  inputElement.id = inputId;
-  parentElement.append(inputElement);
-}
-
-export function createEditInput(inputId, parentElement, oldElement, type, preFilledValue) {
-  const inputElement = document.createElement('input');
-  inputElement.id = inputId;
-  inputElement.type = type;
-  inputElement.value = preFilledValue;
-  parentElement.replaceWith(inputElement, oldElement);
-}
-
-export function createRadioInput(inputClass, parentElement, valueArray) {
-  const elementContainer = document.createElement('div')
-  elementContainer.id = 'radio-container'
-  parentElement.append(elementContainer)
-
-
-}
 
 // функция удаления
+// export function getRemoveButtons(container) {
+//   let removeButtons = container.querySelectorAll('.remove-button')
+//   const removeButtonArray = Array.from(removeButtons)
+//   removeButtons.forEach(button => {
+//       button.addEventListener('click', (e) => {
+//           index = removeButtonArray.indexOf(button)
+//           if(container === projectContainer) {
+//               console.log(e.target)
+//               deleteProject(index)
+//           }
+//           else if(container === taskContainer) {
+//               taskIndex = removeButtonArray.indexOf(button)
+//               projectList[index].deleteTask(taskIndex)
+//           }
+//           renderProjects()
+//           // renderTasks()
+//       })
+//   })
+// }
+
+
+
+// вешаем слушатель событий на кнопку удаления
 export function getRemoveButtons(container) {
   let removeButtons = container.querySelectorAll('.remove-button')
-  const removeButtonArray = Array.from(removeButtons)
-  removeButtons.forEach(button => {
-      button.addEventListener('click', () => {
+  removeButtons.forEach((button, index) => {
+      button.addEventListener('click', (e) => {
           if(container === projectContainer) {
-              index = removeButtonArray.indexOf(button)
-              deleteProject(index)
+            console.log(e.target)
+            deleteProject(index)
           }
           else if(container === taskContainer) {
-              taskIndex = removeButtonArray.indexOf(button)
-              projectList[index].deleteTask(taskIndex)
+            // не решенная задача, как определить индекс - решить после реализации тасков
+            projectList[index].deleteTask(taskIndex)
           }
           renderProjects()
+          
+          // renderTasks()
+      })
+  })
+}
+
+// кнопка редактирования проекта
+export function getEditButtons(container) {
+  let editButtons = container.querySelectorAll('.edit-button')
+  editButtons.forEach((button, index) => {
+      button.addEventListener('click', (e) => {
+          if(container === projectContainer) {
+            console.log(e.target)
+            renderProjectEdit(e)
+          }
+          
+          // renderProjects()
           // renderTasks()
       })
   })
