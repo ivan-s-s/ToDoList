7b10cd72d2bf0bba0e44829da69d9510a5edc8b3
surik.v <i.surovtsev22@gmail.com>
2024-03-19 23:35:46 +0500

    Create edit buttons function

--- src/modules/createButton.js
+++ src/modules/createButton.js
@@ -13,3 +13,14 @@ export function createButton(
   return buttonElement;
 }
 
+export function createEditButtonsContainer(
+  oldElement
+) {
+  const buttonContainer = document.createElement('div');
+  buttonContainer.className = 'button-container';
+
+  createButton(buttonContainer, '', 'button-container-save', 'Save');
+  createButton(buttonContainer, '', 'button-container-cancel', 'Cancel');
+
+  oldElement.replaceWith(buttonContainer);
+}
