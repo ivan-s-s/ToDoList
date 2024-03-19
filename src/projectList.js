fe9e178e19d36238c484a0754809361e2186e78a
surik.v <i.surovtsev22@gmail.com>
2024-03-19 23:39:30 +0500

    Add render project edit form function

--- src/projectList.js
+++ src/projectList.js
@@ -1,16 +1,18 @@
 import { projectList } from "./project";
 import hashSvg from "./assets/hash.svg";
-import { createButton } from "./modules/createButton";
-import { getRemoveButtons } from "./renderDom";
+import { createButton, createEditButtonsContainer } from "./modules/createButton";
+import { getEditButtons, getRemoveButtons } from "./renderDom";
+import { createEditInput, createInput } from "./modules/createInput";
 
 export const projectContainer = document.getElementById('projects-list');
 
 export function renderProjects() {
   projectContainer.innerHTML = '';
 
-  projectList.forEach((project, index) => {
+  projectList.forEach(project => {
     const projectBox = document.createElement('li');
     projectBox.className = 'project';
+    // projectBox.id = project.id;
     projectContainer.append(projectBox);
 
     const projectDiv = document.createElement('div');
@@ -32,11 +34,26 @@ export function renderProjects() {
     const projectButtonContainer = document.createElement('div');
     projectButtonContainer.className = 'button-container';
 
-    createButton(projectButtonContainer, 'project-edit-btn', 'edit-button', 'Edit');
-    createButton(projectButtonContainer, 'project-remove-btn', 'remove-button', 'X');
+    createButton(projectButtonContainer, '', 'edit-button', 'Edit');
+    createButton(projectButtonContainer, '', 'remove-button', 'X');
 
     projectDiv.append(projectButtonContainer);
-
-    getRemoveButtons(projectContainer)
   })
+  getRemoveButtons(projectContainer)
+  getEditButtons(projectContainer)
+}
+
+export function renderProjectEdit(e, index) {
+  console.log('edit start')
+
+  const projectBox = e.target.closest('li');
+  const projectDiv = projectBox.firstChild;
+
+  const titleSpan = projectDiv.childNodes[1];
+  const title = titleSpan.textContent;
+  createEditInput('edit-form-input', title, titleSpan);
+
+  const buttonsDiv = projectDiv.lastChild;
+  
+  createEditButtonsContainer(buttonsDiv);
 }
